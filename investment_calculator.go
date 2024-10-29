7e9680d332e39128f4ca4bb6a3b6954b131d373f
abrahamkifleT <abrahamkifle1212@gmail.com>
2024-10-29 10:18:21 +0300

    add futureReal value

--- investment_calculator.go
+++ investment_calculator.go
@@ -6,10 +6,14 @@ import (
 )
 
 func main() {
-	var investmentAmount, years float64 = 1000, 10
+	const inflationRate = 6.5
+	var investmentAmount float64 = 1000.0
+	years := 10.0
 	expectedReturnRate := 5.5
 
 	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
 	fmt.Println(futureValue)
-	fmt.Println("print test file")
+	fmt.Println(futureRealValue)
 }
